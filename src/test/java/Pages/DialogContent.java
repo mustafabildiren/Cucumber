0a2e7d3a5f6820d81751ab0f253524e701101bea
Mustafa <mustafabildiren94@gmail.com>
2022-08-18 17:49:37 +0300

    Initial commit

---
+++ src/test/java/Pages/DialogContent.java
@@ -0,0 +1,72 @@
+package Pages;
+
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DialogContent extends Parent{
+
+    public DialogContent() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+    @FindBy(id="mat-input-0")
+    private WebElement username;
+
+    @FindBy(id="mat-input-1")
+    private WebElement password;
+
+    @FindBy(css="button[aria-label='LOGIN']")
+    private WebElement loginButton;
+
+    @FindBy(xpath="(//span[contains(text(),'Dashboard')])[2]")
+    private WebElement dashboard;
+
+    WebElement myElement;
+    public void findAndSend(String strElement, String value){  // 2.aşama
+        // burda string isimden weblemente ulaşıcam
+        switch (strElement)
+        {
+            case "username" : myElement =username; break;
+            case "password" : myElement =password; break;
+        }
+
+        sendKeysFunction(myElement, value);
+    }
+
+    public void findAndClick(String strElement){  // 2.aşama
+        // burda string isimden weblemente ulaşıcam
+        switch (strElement)
+        {
+            case "loginButton" : myElement =loginButton; break;
+        }
+
+        clickFunction(myElement);
+    }
+
+    public void findAndContainsText(String strElement, String text){  // 2.aşama
+        // burda string isimden weblemente ulaşıcam
+        switch (strElement)
+        {
+            case "dashboard" : myElement =dashboard; break;
+        }
+
+        verifyContainsText(myElement,text);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
